960619396ea930ffa3745a665db0a65a8f0e7269
nikhil2005trx <149887804+nikhil2005trx@users.noreply.github.com>
2026-07-18 21:29:57 +0530

    Add Cache in my docker file

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  output: "standalone",
+  /* config options here */
+};
 
 export default nextConfig;
